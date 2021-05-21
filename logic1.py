a79fbcf44c0bc3cf95b5af92f947c502989dac08
Dicky Giancini <dicky.giancini.123@gmail.com>
2021-05-21 19:42:17 +0700

    Logic Test Genesys

---
+++ logic1.py
@@ -0,0 +1,21 @@
+x = 8
+
+for i in range(1, x):
+    for j in range(i):
+        print(j)
+
+
+# x = 5
+# y = 1
+# z = 1
+# a = x
+# b = 5
+# while x >= 0:
+#     while a >= 0:
+#         print(" ")
+#         a -= 1
+#     while y < b-x:
+#         print("*")
+#         y += 1
+#     x -= 1
+#     print("\n")
