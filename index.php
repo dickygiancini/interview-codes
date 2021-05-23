7d655e53fff739def130b6d8d81f955cade01de5
Dicky Giancini <dicky.giancini.123@gmail.com>
2021-05-23 21:41:42 +0700

    fibonacci

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $a = 1;
+    $b = 1;
+    $temp = 0;
+
+    for ($i = 1; $i <= 10; $i++) {
+        echo $a . " ";
+        $temp = $a + $b;
+        $a = $b;
+        $b = $temp;
+    }
+
+    ?>
+</body>
+
+</html>
